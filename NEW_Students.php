c247a10aec0a2f76b6cb46cbe6a27d947cfc2070
SWI-MIN <swimming881127@gmail.com>
2020-05-31 23:00:03 +0800

    NEW_Student

--- NEW_Students.php
+++ NEW_Students.php
@@ -1,33 +1,45 @@
+<?php
+    header("Content-type: text/html; charset=utf-8");//頁面編碼
+    $dbms='mysql';     //数据库类型
+    $host='localhost'; //数据库主机名
+    $dbName='dbms_project';    //使用的数据库
+    $user='root';      //数据库连接用户名
+    $pass='';          //对应的密码
+    $dsn="$dbms:host=$host;dbname=$dbName";
 
-        <?php
-            header("Content-type: text/html; charset=utf-8");//頁面編碼
-            $dbms='mysql';     //数据库类型
-            $host='localhost'; //数据库主机名
-            $dbName='dbms_project';    //使用的数据库
-            $user='root';      //数据库连接用户名
-            $pass='';          //对应的密码
-            $dsn="$dbms:host=$host;dbname=$dbName";
-            
 
-            $stuid = $_POST['stuid'];                              //取得USER輸入的id
-            $pwd = $_POST['pwd'];                                   //取得USER輸入的password
-            $stuname = $_POST['stuname']; 
-            $studepart = $_POST['studepart'];                              
-            $stufloor = $_POST['stufloor'];                                   
-            $stuclass = $_POST['stuclass'];
+    $stuid = $_POST['stuid'];                              //取得USER輸入的id
+    $pwd = $_POST['pwd'];                                   //取得USER輸入的password
+    $stuname = $_POST['stuname']; 
+    $studepart = $_POST['studepart'];                              
+    $stufloor = $_POST['stufloor'];                                   
+    $stuclass = $_POST['stuclass'];
 
-            if ($stuid == '' || $pwd == '' || $stuname == '' || $studepart == '' || $stufloor == '' || $stuclass == ''){
-                echo '<script>alert("通通不能為空!!");history.go(-1);</script>';
-                exit(0);
-            } else {
-                $conn = new mysqli($dsn, $user, $pass, $options);
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                  }
-                $sql = "INSERT INTO students (stuid, pwd, stuname, studepart, stufloor, stuclass)
-                 VALUES ($stuid, $pwd, $stuname, $studepart, $stufloor, $stuclass);";
-                $conn->prepare($sql)->execute([$stuid, $pwd, $stuname, $studepart, $stufloor, $stuname]);
-                
-            }  
-            
-        ?>
+    $conn = new mysqli($host, $user, $pass, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+    if ($stuid == '' || $pwd == '' || $stuname == '' || $studepart == '' || $stufloor == '' || $stuclass == ''){
+        echo '<script>alert("通通不能為空!!");history.go(-1);</script>';
+        exit(0);
+    } else {
+        $sql = "INSERT INTO students (stuid, pwd, stuname, studepart, stufloor, stuclass)
+            VALUES ('$stuid', '$pwd', '$stuname', '$studepart', '$stufloor', '$stuclass' ); ";
+        if ($conn->query($sql)) {           // query() 判斷資料庫查詢是否成功，if 成功回傳 true，else 回傳 false
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;//前方的error相當於 mysql_error();，用於迴船錯誤訊息
+        }
+    }  
+
+
+
+
+
+
+
+
+    mysql_close($conn);
+?>
