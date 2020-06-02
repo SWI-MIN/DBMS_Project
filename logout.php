9d29c6b5145c89f525f069f8e1251ee9f26c843a
SWI-MIN <swimming881127@gmail.com>
2020-06-02 20:00:41 +0800

    終於取到值了啦!!!!!!!!!

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
     header("Content-type:text/html;charset=utf-8");
     session_start();
     if($logout){
-        unset($_SESSION['echo']);//直接unset，或可用session里面的函数
+        unset($_SESSION['userData']);//直接unset，或可用session里面的函数
         echo "<script>window.location.href='login_index.php';</script>";
     }
     
